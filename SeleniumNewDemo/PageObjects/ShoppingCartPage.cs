d01bf215b982c69a4dd6b171dbcbf498f417d320
Sachielsc <sachielsc@gmail.com>
2018-04-19 16:49:13 +1200

    Selenium Webdriver (POM only) a slim and further version of "Charles Selenium Webdriver"

---
+++ SeleniumNewDemo/PageObjects/ShoppingCartPage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMExample.PageObjects
+{
+    class ShoppingCartPage
+	{
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+		[FindsBy(How = How.CssSelector, Using = "div.totals>div.terms-of-service>input#termsofservice")]
+		private IWebElement termOfService;
+
+		[FindsBy(How = How.CssSelector, Using = "div.checkout-buttons button#checkout")]
+		private IWebElement checkoutButton;
+
+		public ShoppingCartPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+            PageFactory.InitElements(driver, this);
+        }
+
+		public void AcceptTermOfService()
+		{
+			termOfService.Click();
+		}
+
+		public CheckoutPage GoToCheckoutPage()
+		{
+			wait.Until(ExpectedConditions.ElementToBeClickable(checkoutButton));
+			checkoutButton.Click();
+			return new CheckoutPage(driver);
+		}
+	}
+}
---
+++ SeleniumNewDemo/PageObjects/ThankYouPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMExample.PageObjects
+{
+    class ThankYouPage
+	{
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+		[FindsBy(How = How.CssSelector, Using = "div.order-completed input.order-completed-continue-button")]
+		private IWebElement continueButton;
+
+		public ThankYouPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+            PageFactory.InitElements(driver, this);
+        }
+
+		public HomePage FinishPurchaseAndGoToHomePage()
+		{
+			wait.Until(ExpectedConditions.ElementToBeClickable(continueButton));
+			continueButton.Click();
+			return new HomePage(driver);
+		}
+	}
+}
